a759a3bc0859ee88554b46d3d0723ccf3fb1d8b4
bulotge <bulotge@gisis.eu>
2020-01-13 15:48:48 +0100

    Formatting BlogPost list, formatting time (timeago.js)

--- src/components/BlogPostList.js
+++ src/components/BlogPostList.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import timeago from 'timeago.js';
 
 class BlogPostList extends Component {
     render() {
@@ -19,7 +20,18 @@ class BlogPostList extends Component {
         return (
             <div>
                 <ul>
-                    {posts && posts.map(post => (<li key={post.id}>{post.title}</li>))}
+                    {posts && posts.map(post => (
+                        <div key={post.id} className="card mb-3 mt-3 shadow-sm">
+                            <div className="card-body">
+                                <h4>{post.title}</h4>
+                                <p className="card-text bordet-top">
+                                    <small className="text-muted">
+                                        {timeago().format(post.published)}
+                                    </small>
+                                </p>
+                            </div>
+                        </div>
+                    ))}
                 </ul>
             </div>
         );
