d37faf20202f878576671c99378926a87853e034
bulotge <bulotge@gisis.eu>
2020-01-14 15:43:41 +0100

    Redux Form submission, handling login errors

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -15,8 +15,6 @@ const mapDispatchToProps = {
 class LoginForm extends Component {
     componentDidUpdate(prevProps, prevState, snapshot) {
         if (prevProps.token !== this.props.token) {
-            console.log(prevProps);
-            console.log(this.props);
             this.props.history.push('/');
         }
     }
@@ -28,10 +26,15 @@ class LoginForm extends Component {
     }
 
     render() {
-        const { handleSubmit } = this.props;
+        const { handleSubmit, error } = this.props;
+
+        console.log(error);
 
         return (
             <div className="text-center">
+                {error &&
+                    <div className="alert alert-danger">{error}</div>
+                }
                 <form className="mt-4" onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                     <Field name="username"  label="Username" type="text" component={renderField} />
                     <Field name="password" label="Password" type="password" component={renderField} />
