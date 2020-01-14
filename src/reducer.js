eef6e6fba7d8149c35463374cfaae5bae20c6620
bulotge <bulotge@gisis.eu>
2020-01-14 14:03:18 +0100

    Storing JWT token in LocalStorage, Token Middleware

--- src/reducer.js
+++ src/reducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
+import {reducer as formReducer} from 'redux-form';
 import blogPostList from "./reducers/BlogPostList";
 import blogPost from "./reducers/blogPost";
 import commentList from "./reducers/commentList";
-import {reducer as formReducer} from 'redux-form';
+import auth from "./reducers/auth";
 
 export default  combineReducers({
     blogPostList,
     blogPost,
     commentList,
+    auth,
     form: formReducer
 });
