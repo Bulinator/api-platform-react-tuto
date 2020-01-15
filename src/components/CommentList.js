59f1c798e3c806898b29e4e70b357472a2b0350c
bulotge <bulotge@gisis.eu>
2020-01-15 11:02:34 +0100

    Animations - animated comment adding

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import timeago from 'timeago.js';
 import { Message } from "./common/Message";
 
+import "./CommentList.css";
 
 class CommentList extends Component {
     render() {
@@ -15,20 +17,24 @@ class CommentList extends Component {
 
         return (
             <div className="card shadow-sm">
-                {commentList.map(comment => {
-                    return (
-                        <div className="card-body border-bottom" key={comment.id} >
-                            <p className="card-text mb-0">
-                                <small>{comment.content}</small>
-                            </p>
-                            <p className="card-text">
-                                <small className="text-muted">
-                                    {timeago().format(comment.published)} by {comment.author.name}
-                                </small>
-                            </p>
-                        </div>
-                    );
-                })}
+                <TransitionGroup>
+                    {commentList.map(comment => {
+                        return (
+                            <CSSTransition key={comment.id} timeout={500} classNames="fade">
+                                <div className="card-body border-bottom">
+                                    <p className="card-text mb-0">
+                                        <small>{comment.content}</small>
+                                    </p>
+                                    <p className="card-text">
+                                        <small className="text-muted">
+                                            {timeago().format(comment.published)} by {comment.author.name}
+                                        </small>
+                                    </p>
+                                </div>
+                            </CSSTransition>
+                        );
+                    })}
+                </TransitionGroup>
             </div>
         );
     }
--- src/reducers/commentList.js
+++ src/reducers/commentList.js
@@ -2,10 +2,8 @@ import {
     COMMENT_LIST_REQUEST,
     COMMENT_LIST_RECEIVED,
     COMMENT_LIST_ERROR,
-    COMMENT_LIST_UNLOAD
+    COMMENT_LIST_UNLOAD, COMMENT_ADDED
 } from "../actions/types";
-
-
 
 export default(state = {commentList: null, isFetching: false}, action) => {
     switch (action.type) {
@@ -19,6 +17,11 @@ export default(state = {commentList: null, isFetching: false}, action) => {
                 ...state,
                 commentList: action.data['hydra:member'],
                 isFetching: false
+            };
+        case COMMENT_ADDED:
+            return {
+                ...state,
+                commentList: [action.comment, ...state.commentList]
             };
         case COMMENT_LIST_ERROR:
         case COMMENT_LIST_UNLOAD:
