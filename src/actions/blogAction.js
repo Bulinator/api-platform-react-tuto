f0cc5b2e14fa298f3f37bed9b4e8f86d3887339c
bulotge <bulotge@gisis.eu>
2020-01-16 16:56:55 +0100

    Functional BlogPost form

--- src/actions/blogAction.js
+++ src/actions/blogAction.js
@@ -7,9 +7,14 @@ import {
     BLOG_POST_REQUEST,
     BLOG_POST_ERROR,
     BLOG_POST_UNLOAD,
-    BLOG_POST_LIST_SET_PAGE
+    BLOG_POST_LIST_SET_PAGE,
+    BLOG_POST_FORM_UNLOAD
 } from "./types";
 import {requests} from "../agent";
+import {userLogout} from "./userAction";
+import {SubmissionError} from "redux-form";
+import {parseApiErrors} from "../utils/apiUtils";
+import {commentAdded} from "./commentAction";
 
 export const blogPostListRequest = () => ({
     type: BLOG_POST_LIST_REQUEST
@@ -30,13 +35,13 @@ export const blogPostListSetPage = (page) => ({
     page
 });
 
-export const blogPostAdd = () => ({
-    type: BLOG_POST_LIST_ADD,
-    data: {
-        id: Math.floor(Math.random() * 100 + 3),
-        title: 'A newly added blog post'
-    }
-});
+// export const blogPostAdd = () => ({
+//     type: BLOG_POST_LIST_ADD,
+//     data: {
+//         id: Math.floor(Math.random() * 100 + 3),
+//         title: 'A newly added blog post'
+//     }
+// });
 
 export const blogPostListFetch = (page = 1) => {
     return (dispatch) => {
@@ -73,3 +78,29 @@ export const blogPostReceived = (data) => ({
 export const blogPostUnload = () => ({
    type: BLOG_POST_UNLOAD
 });
+
+export const blogPostFormUnload = () => ({
+    type: BLOG_POST_FORM_UNLOAD
+});
+
+export const blogPostAdd = (title, content) => {
+    return (dispatch) => {
+        return requests.post(
+            '/blog_posts',
+            {
+                title,
+                content,
+                slug: title && title.replace(/ /g, "-").toLowerCase()
+            }
+        ).catch((error) => {
+            if (401 === error.response.status) {
+                return dispatch(userLogout());
+            } else if (403 === error.response.status) {
+                throw new SubmissionError({
+                    _error: 'You do not have rights to publish blog posts!'
+                });
+            }
+            throw new SubmissionError(parseApiErrors(error));
+        })
+    }
+};
